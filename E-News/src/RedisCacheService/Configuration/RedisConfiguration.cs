95b4e0272630b9090676e64e954647c4a31c2f1c
Yusuf <yusufyilmazfr@gmail.com>
2020-12-05 17:18:18 +0300

    Redis cache configuration

---
+++ E-News/src/RedisCacheService/Configuration/RedisConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCacheService.Configuration
+{
+    /// <summary>
+    /// Redis server configuration informations
+    /// </summary>
+    public class RedisConfiguration
+    {
+        /// <summary>
+        /// Redis server hostname
+        /// </summary>
+        public string Host { get; set; }
+        /// <summary>
+        /// Redis server password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Redis server port
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
