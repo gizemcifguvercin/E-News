7a192c6038ebd86779cefd0970886935c88378cc
Yusuf <yusufyilmazfr@gmail.com>
2020-12-05 17:27:47 +0300

    Service extension methods was defined for registering dependencies

---
+++ E-News/src/RedisCacheService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using RedisCacheService.CacheService;
+using RedisCacheService.Configuration;
+
+namespace RedisCacheService.Extensions
+{
+    /// <summary>
+    /// Add Redis cache service dependencies
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register Redis dependencies to IServiceCollection
+        /// </summary>
+        /// <param name="services">IServiceCollection instance</param>
+        /// <param name="configuration">RedisConfiguration instance</param>
+        /// <returns></returns>
+        public static IServiceCollection AddRedisCacheService(this IServiceCollection services, Func<RedisConfiguration> configuration)
+        {
+            RedisConfiguration redisConfigurationInstance = configuration.Invoke();
+
+            services.AddSingleton<IRedisCacheService, CacheService.RedisCacheService>();
+            services.AddSingleton<RedisConfiguration>(redisConfigurationInstance);
+
+            return services;
+        }
+    }
+}
