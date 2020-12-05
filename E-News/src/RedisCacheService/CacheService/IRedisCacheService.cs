5aeeaa135253a31ee7a72f63188523eb819d65a5
Yusuf <yusufyilmazfr@gmail.com>
2020-12-05 17:19:19 +0300

    Redis cache service implementation has done

---
+++ E-News/src/RedisCacheService/CacheService/IRedisCacheService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCacheService.CacheService
+{
+    public interface IRedisCacheService
+    {
+        /// <summary>
+        /// Insert data to cache.
+        /// </summary>
+        /// <param name="key">Unique key name</param>
+        /// <param name="data">Data</param>
+        /// <param name="expiry">Expiration minutes</param>
+        /// <param name="databaseId">Current database id</param>
+        void Add(string key, object data, int expiry = 60, int databaseId = 0);
+        /// <summary>
+        /// Get data from cache.
+        /// </summary>
+        /// <typeparam name="T">Return element type</typeparam>
+        /// <param name="key">Unique key name</param>
+        /// <param name="databaseId">Current database id</param>
+        /// <returns></returns>
+        T Get<T>(string key, int databaseId = 0);
+        /// <summary>
+        /// Remove element from cache.
+        /// </summary>
+        /// <param name="key">Unique key name</param>
+        /// <param name="databaseId">Current database id</param>
+        void Remove(string key, int databaseId = 0);
+        /// <summary>
+        /// Insert data to cache.
+        /// </summary>
+        /// <param name="hashKey">Hash key name</param>
+        /// <param name="key">Unique key name</param>
+        /// <param name="data">Data</param>
+        /// <param name="databaseId">Current database id</param>
+        void AddHash(string hashKey, string key, object data, int databaseId = 0);
+        /// <summary>
+        /// Get data from cache.
+        /// </summary>
+        /// <typeparam name="T">Return element type</typeparam>
+        /// <param name="hashKey">Hash key name</param>
+        /// <param name="key">Unique key name</param>
+        /// <param name="databaseId">Current database id</param>
+        /// <returns></returns>
+        T GetHash<T>(string hashKey, string key, int databaseId = 0);
+        /// <summary>
+        /// Remove element from cache.
+        /// </summary>
+        /// <param name="hashKey">Hash key name</param>
+        /// <param name="key">Unique key name</param>
+        /// <param name="databaseId">Current database id</param>
+        void RemoveHash(string hashKey, string key, int databaseId = 0);
+    }
+}
---
+++ E-News/src/RedisCacheService/CacheService/RedisCacheService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RedisCacheService.Server;
+
+namespace RedisCacheService.CacheService
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly RedisServer _redisServer;
+
+        public RedisCacheService(RedisServer redisServer)
+        {
+            _redisServer = redisServer;
+        }
+
+        public void Add(string key, object data, int expiry = 60, int databaseId = 0)
+        {
+            string parsedJsonData = JsonConvert.SerializeObject(data);
+
+            _redisServer
+                .GetDatabase(databaseId)
+                .StringSet(key, parsedJsonData, TimeSpan.FromMinutes(expiry));
+        }
+
+        public T Get<T>(string key, int databaseId = 0)
+        {
+            string parsedJsonData = _redisServer.GetDatabase(databaseId).StringGet(key);
+            return JsonConvert.DeserializeObject<T>(parsedJsonData);
+        }
+
+        public void Remove(string key, int databaseId = 0)
+        {
+            _redisServer.GetDatabase(databaseId).KeyDelete(key);
+        }
+
+        public void AddHash(string hashKey, string key, object data, int databaseId = 0)
+        {
+            string parsedJsonData = JsonConvert.SerializeObject(data);
+
+            _redisServer
+                .GetDatabase(databaseId)
+                .HashSet(hashKey, key, parsedJsonData);
+        }
+
+        public T GetHash<T>(string hashKey, string key, int databaseId = 0)
+        {
+            string parsedJsonData = _redisServer.GetDatabase(databaseId).HashGet(hashKey, key);
+            return JsonConvert.DeserializeObject<T>(parsedJsonData);
+        }
+
+        public void RemoveHash(string hashKey, string key, int databaseId = 0)
+        {
+            _redisServer.GetDatabase(databaseId).HashDelete(hashKey, key);
+        }
+    }
+}
